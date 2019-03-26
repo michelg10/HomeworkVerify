e5311f04925b019deb8b5281b514e27fe34afc7c
LegitMichel777 <legitmichel777@legitmichel777s-imac.local>
2019-03-26 21:14:08 +0800

    Initial Commit

---
+++ HomeworkVerify/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HomeworkVerify
+//
+//  Created by LegitMichel777 on 3/26/19.
+//  Copyright © 2019 LegitMichel777. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
